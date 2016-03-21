40476816e0db2bf1a7a0c784f1e3bcfc7a8acafd
tilana <nataliewidmann@gmx.de>
2016-03-21 10:07:51 +0100

    added Test for createDictionary in documentCollection

--- Unittests/testDocumentCollection.py
+++ Unittests/testDocumentCollection.py
@@ -1,22 +1,33 @@
 import unittest
 import os, sys
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath('documentCollection'))))
+t = sys.path.append(os.path.dirname(os.path.abspath('LDA')))
 from documentCollection import documentCollection
 
 class testDocumentCollection(unittest.TestCase):
-   
-    collection = documentCollection(' ')
-    collection.titles = ['doc1','doc2','doc3']
-    collection.documents = ['document collection to test named entity recognition.',' African Commission is a named entity, also countries like Senegal and Lybia and names like Peter and Anna.','Also organizations like the United Nations or UNICEF should be recognized.']
     
+    def setUp(self):
+        self.collection = documentCollection(' ')
+        self.collection.titles = ['doc1','doc2','doc3']
+                
     def test(self):
         self.assertTrue(True)
-
+    
+    def test_createDictionary(self):
+        self.collection.documents = ['Test -tokenization- and if common words are deleted.', 'stopwords like of, and, but?','special\ characters?\n, Article\n\n78(5), constitution references 103/93 and dates 23.01.1998 or 12th of March 2003']
+        self.collection.createDictionary(rmStopwords=False)
+        self.assertEqual(self.collection.dictionary, set(['test', '-tokenization-', 'and', 'if','common', 'words','are','deleted','.','stopwords','like','of',',','but','?','special\\','characters','article','78','(','5',')','constitution','references','103/93','dates','23.01.1998','or', '12th','march','2003']))
+        self.collection.createDictionary()
+        self.assertEqual(self.collection.dictionary, set(['test','like','-tokenization-','common','words','deleted','.','stopwords',',','?','special\\','characters','article','78','(','5',')','constitution','references','103/93','78','dates','23.01.1998','12th','march','2003']))
+       
     def test_getNamedEntities(self):
-        collection.getNamedEntities()
+        self.collection.documents = ['document collection to test named entity recognition.',' African Commission is a named entity, also countries like Senegal and Lybia and names like Peter and Anna.','Also organizations like the United Nations or UNICEF should be recognized.']
         testEntities = [set([]), set(['African Commission', 'Senegal', 'Lybia', 'Peter', 'Anna']), set(['United Nations', 'UNICEF'])]
-        self.assertEqual(testEntities, collection.namedEntities)
+        self.collection.getNamedEntities()
+        self.assertEqual(testEntities, self.collection.namedEntities)
+    
+    def tearDown(self):
+        del self.collection
 
 if __name__ == '__main__':
     unittest.main()
--- nlpProcessing.py
+++ nlpProcessing.py
@@ -9,82 +9,79 @@ import utils
 
 # tokenize collection of document to word level
 def tokenizeColl(coll):
-	return set(utils.flattenList([nltk.word_tokenize(doc) for doc in coll]))
+    dictionary = utils.flattenList([nltk.word_tokenize(doc) for doc in coll])
+    return set([words.lower() for words in dictionary])
 
 # tokenize a document on sentence and word level
 def tokenize(doc):
-	sentences = nltk.sent_tokenize(doc)
-	words = [nltk.word_tokenize(sent) for sent in sentences]
-	return words
+    sentences = nltk.sent_tokenize(doc)
+    return [nltk.word_tokenize(sent) for sent in sentences]
 
 # tag parts of speech and chunk named entities 
 def posTag(doc):
-	pos = [nltk.pos_tag(words) for words in doc]
-	return [nltk.ne_chunk(tag, binary=True) for tag in pos]
+    pos = [nltk.pos_tag(words) for words in doc]
+    return [nltk.ne_chunk(tag, binary=True) for tag in pos]
 
 # traverse through syntax tree
 def traverse(tree):
-	namedEntities = []
-	
-	if hasattr(tree, 'label') and tree.label:
-		if tree.label() == 'NE':
-			namedEntities.append(' '.join([child[0] for child in tree]))
-		else:
-			for child in tree:
-				namedEntities.extend(traverse(child))
-	return namedEntities
-
+    namedEntities = []
+    if hasattr(tree, 'label') and tree.label:
+        if tree.label() == 'NE':
+            namedEntities.append(' '.join([child[0] for child in tree]))
+        else:
+            for child in tree:
+                namedEntities.extend(traverse(child))
+    return namedEntities
 
 # Named-entity recognition
 def namedEntityRecognizer(pos):
-	namedEntities = []
-	for tag in pos:
-		namedEntities.extend(traverse(tag))
-	return set(namedEntities)
+    namedEntities = []
+    for tag in pos:
+        namedEntities.extend(traverse(tag))
+    return set(namedEntities)
 
 # get named Enities
 def getNamedEntities(coll):
-	wordTokens = [tokenize(doc) for doc in coll]
-	pos = [posTag(tokens) for tokens in wordTokens]
-	return [namedEntityRecognizer(p) for p in pos]
+    wordTokens = [tokenize(doc) for doc in coll]
+    pos = [posTag(tokens) for tokens in wordTokens]
+    return [namedEntityRecognizer(p) for p in pos]
 
 
 # remove stopwords from dictionary
 def removeStopwords(doc):
-	return set([word.lower() for word in doc if word.lower() not in STOPWORDS])
+    return set([word.lower() for word in doc if word.lower() not in STOPWORDS])
 
 # remove stopwords from dictionary
 def removeStopwordsDoc(doc):
-	return [word for word in doc.lower().split() if word not in STOPWORDS and word.isalnum()] 
+    return [word for word in doc.lower().split() if word not in STOPWORDS and word.isalnum()] 
 
 # save preprocessed data to file
 def saveProcessedDocs(dat, name):
-	with open(name, 'wb') as f:
-		pickle.dump(dat, f)
+    with open(name, 'wb') as f:
+        pickle.dump(dat, f)
 
 # remove all occurences of words in l1 one which are part of l2
 def removeWords(dic, namedEntities):
-	namedEntityList = splitNamedEntities(namedEntities)
-	intersect = [word for word in dic if word not in namedEntityList]
-	delWord = [word for word in dic if word in namedEntityList]
-	return namedEntities.union(set(intersect)), set(delWord)
+    namedEntityList = splitNamedEntities(namedEntities)
+    intersect = [word for word in dic if word not in namedEntityList]
+    delWord = [word for word in dic if word in namedEntityList]
+    return namedEntities.union(set(intersect)), set(delWord)
 
 # transform namedEntities to list of splitted characters
 def splitNamedEntities(namedEntity):
-	splittedList = [ent[1].lower().split() for ent in enumerate(namedEntity)]
-	return [item for sublist in splittedList for item in sublist]
+    splittedList = [ent[1].lower().split() for ent in enumerate(namedEntity)]
+    return [item for sublist in splittedList for item in sublist]
 
 # get word frequency in document
 def wordFreqInDoc(word, doc):
-	return tb(doc.lower()).words.count(word.lower()) 
+    return tb(doc.lower()).words.count(word.lower()) 
 
 # get number of documents containing a word
 def wordFreqInColl(word, docList):
-	return sum(1 for doc in docList if word.lower() in doc.lower())
+    return sum(1 for doc in docList if word.lower() in doc.lower())
 
 # filter words based on Frequency
 def filterDict(docs, words, minDocFreq=2, minCollFreq=2, minWordLength=1):
-	lemmatizer = WordNetLemmatizer()
-	return [lemmatizer.lemmatize(word) for word in words if wordFreqInColl(word, docs)>minCollFreq and len(word)>minWordLength]
-
+    lemmatizer = WordNetLemmatizer()
+    return [lemmatizer.lemmatize(word) for word in words if wordFreqInColl(word, docs)>minCollFreq and len(word)>minWordLength]
 
