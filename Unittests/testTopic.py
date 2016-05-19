b22fee61ce28df04ab322b84d22846a16aae462c
tilana <nataliewidmann@gmx.de>
2016-05-19 08:00:07 +0200

    update Unittest/testTopic.py

--- Unittests/testTopic.py
+++ Unittests/testTopic.py
@@ -7,6 +7,7 @@ class testTopic(unittest.TestCase):
     def setUp(self):
         self.testTopic = Topic()
         self.targetTopic = Topic()
+        self.word2vec = Word2Vec()
 
     def test_addTopic(self):
         self.testTopic.addTopic((0, [(u'Test', -0.31), (u'Words', 0.045), (u'in', -0.23), (u'Topic', -0.002)])) 
@@ -17,22 +18,33 @@ class testTopic(unittest.TestCase):
         self.assertTrue(self.targetTopic.__eq__(self.testTopic))
 
     def test_labelTopic(self):
-        word2vec = Word2Vec()
         categories = ['computer', 'food', 'books', 'fruit', 'politics']
         self.testTopic.wordDistribution = [(u'apple', 0.32), (u'pear', 0.22), (u'strawberry', -0.20), ('banana', 0.1), ('cherry', 0.11)]
         targetCategories1 = ['fruit', 'food', 'books', 'computer', 'politics']
-        self.testTopic.labelTopic(word2vec, categories)
+        self.testTopic.labelTopic(self.word2vec, categories)
         self.assertEqual(targetCategories1, self.testTopic.keywords)
 
         self.testTopic.wordDistribution = [(u'mouse', 0.32), (u'power', 0.22), (u'computation', -0.20), ('screen', 0.1)] 
         targetCategories2 = ['computer', 'fruit', 'food', 'books', 'politics']
-        self.testTopic.labelTopic(word2vec, categories)
+        self.testTopic.labelTopic(self.word2vec, categories)
         self.assertEqual(targetCategories2, self.testTopic.keywords)
         
         self.testTopic.wordDistribution = [(u'president', 0.32), (u'constitution', 0.22), (u'discussion', -0.20), ('election', 0.1)] 
         targetCategories3 = ['politics', 'computer', 'food', 'books', 'fruit'] 
-        self.testTopic.labelTopic(word2vec, categories)
+        self.testTopic.labelTopic(self.word2vec, categories)
         self.assertEqual(targetCategories3, self.testTopic.keywords)        
+
+    def test_evaluate(self):
+
+        self.testTopic.wordDistribution = [(u'president', 0.32), (u'constitution', 0.22), (u'discussion', -0.20), ('election', 0.1)] 
+        self.targetTopic.wordDistribution = [(u'president', 0.32), (u'banana', 0.22), (u'discussion', -0.20), ('election', 0.1)] 
+
+        self.testTopic.evaluate(self.word2vec)
+        self.targetTopic.evaluate(self.word2vec)
+
+        self.assertGreater(self.testTopic.meanSimilarity, self.targetTopic.meanSimilarity)
+
+
 
 
 if __name__ == '__main__':
